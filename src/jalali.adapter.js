95a6b809ea750fbfa6d72b7489b60fe3eef74460
Dariush Alipour <drsh.alipour@gmail.com>
2017-04-22 15:45:52 +0430

    refactored code style

--- src/jalali.adapter.js
+++ src/jalali.adapter.js
@@ -1,58 +1,57 @@
 const jalaaliJs = require('jalaali-js');
 
-const months = [
-  { name: 'Farvardin', short: 'Farv' },
-  { name: 'Ordibehesht', short: 'Ord' },
-  { name: 'Khordad', short: 'Kho' },
-  { name: 'Tir', short: 'Tir' },
-  { name: 'Mordad', short: 'Mor' },
-  { name: 'Shahrivar', short: 'Sha' },
-  { name: 'Mehr', short: 'Mehr' },
-  { name: 'Aban', short: 'Aban' },
-  { name: 'Azar', short: 'Azar' },
-  { name: 'Dey', short: 'Dey' },
-  { name: 'Bahman', short: 'Bah' },
-  { name: 'Esfand', short: 'Esf' },
-];
+const Adapter = function Adapter() {
+  const name = 'dariush-alipour.onecalendar.adapter.jalali';
 
-exports.name = 'dariush-alipour.onecalendar.adapter.jalali';
+  const months = [
+    { name: 'Farvardin', short: 'Farv' },
+    { name: 'Ordibehesht', short: 'Ord' },
+    { name: 'Khordad', short: 'Kho' },
+    { name: 'Tir', short: 'Tir' },
+    { name: 'Mordad', short: 'Mor' },
+    { name: 'Shahrivar', short: 'Sha' },
+    { name: 'Mehr', short: 'Mehr' },
+    { name: 'Aban', short: 'Aban' },
+    { name: 'Azar', short: 'Azar' },
+    { name: 'Dey', short: 'Dey' },
+    { name: 'Bahman', short: 'Bah' },
+    { name: 'Esfand', short: 'Esf' },
+  ];
 
-exports.l10n = function l10n(date) {
-  const newDate = jalaaliJs.toJalaali(date.year, date.month, date.day);
-  const ldate = {
-    year: newDate.jy,
-    month: newDate.jm,
-    day: newDate.jd,
+  const l10n = (date) => {
+    const newDate = jalaaliJs.toJalaali(date.year, date.month, date.day);
+    const ldate = {
+      year: newDate.jy,
+      month: newDate.jm,
+      day: newDate.jd,
+    };
+    return ldate;
   };
-  return ldate;
-};
-
-exports.i18n = function i18n(ldate) {
-  const newDate = jalaaliJs.toGregorian(ldate.year, ldate.month, ldate.day);
-  const date = {
-    year: newDate.gy,
-    month: newDate.gm,
-    day: newDate.gd,
+  const i18n = (ldate) => {
+    const newDate = jalaaliJs.toGregorian(ldate.year, ldate.month, ldate.day);
+    const date = {
+      year: newDate.gy,
+      month: newDate.gm,
+      day: newDate.gd,
+    };
+    return date;
   };
-  return date;
+
+  const getMonthName = (month, short) => {
+    const mon = (months[month - 1]);
+    if (typeof mon === 'undefined') {
+      throw new Error('Invalid month number, number should be between 1 and 12');
+    }
+    return short ? mon.short : mon.name;
+  };
+
+  const getMonthLength = (year, month) => jalaaliJs.jalaaliMonthLength(year, month);
+
+  const isValid = date => jalaaliJs.isValidJalaaliDate(date.year, date.month, date.day);
+
+  const isLeap = year => jalaaliJs.isLeapJalaaliYear(year);
+
+  return { name, l10n, i18n, isValid, isLeap, getMonthName, getMonthLength };
 };
 
-exports.isValid = function isValid(date) {
-  return jalaaliJs.isValidJalaaliDate(date.year, date.month, date.day);
-};
-
-exports.isLeap = function isLeap(year) {
-  return jalaaliJs.isLeapJalaaliYear(year);
-};
-
-exports.getMonthName = function getMonthName(month, short) {
-  const mon = (months[month - 1]);
-  if (typeof mon === 'undefined') {
-    throw new Error('Invalid month number, number should be between 1 and 12');
-  }
-  return short ? mon.short : mon.name;
-};
-
-exports.getMonthLength = function getMonthLength(year, month) {
-  return jalaaliJs.jalaaliMonthLength(year, month);
-};
+exports.Adapter = Adapter;
--- test/jalali.adapter.test.js
+++ test/jalali.adapter.test.js
@@ -1,89 +1,91 @@
 /* eslint-disable no-unused-expressions */
 const expect = require('chai').expect;
-const adapter = require('../src/jalali.adapter.js');
+const JalaliAdapter = require('../src/jalali.adapter.js').Adapter;
+
+const jalaliAdapter = new JalaliAdapter();
 
 describe('Jalali Adapter', () => {
   describe('name', () => {
     it('should return a string', () => {
-      expect(adapter.name).to.be.a('string');
+      expect(jalaliAdapter.name).to.be.a('string');
     });
   });
 
   describe('l10n', () => {
     it('should return {year: 1396, month: 1, day: 31} when (2017, 4, 20)', () => {
       const date = { year: 2017, month: 4, day: 20 };
-      expect(adapter.l10n(date)).to.deep.equal({ year: 1396, month: 1, day: 31 });
+      expect(jalaliAdapter.l10n(date)).to.deep.equal({ year: 1396, month: 1, day: 31 });
     });
     it('should return {year: 1396, month: 2, day: 1} when (2017, 4, 21)', () => {
       const date = { year: 2017, month: 4, day: 21 };
-      expect(adapter.l10n(date)).to.deep.equal({ year: 1396, month: 2, day: 1 });
+      expect(jalaliAdapter.l10n(date)).to.deep.equal({ year: 1396, month: 2, day: 1 });
     });
   });
 
   describe('i18n', () => {
     it('should return {year: 2017, month: 4, day: 20} when (1396, 1, 31)', () => {
       const date = { year: 1396, month: 1, day: 31 };
-      expect(adapter.i18n(date)).to.deep.equal({ year: 2017, month: 4, day: 20 });
+      expect(jalaliAdapter.i18n(date)).to.deep.equal({ year: 2017, month: 4, day: 20 });
     });
     it('should return {year: 2017, month: 4, day: 21} when (1396, 2, 1)', () => {
       const date = { year: 1396, month: 2, day: 1 };
-      expect(adapter.i18n(date)).to.deep.equal({ year: 2017, month: 4, day: 21 });
+      expect(jalaliAdapter.i18n(date)).to.deep.equal({ year: 2017, month: 4, day: 21 });
     });
   });
 
   describe('isValid', () => {
     it('should return false when input is (1394, 12, 30)', () => {
       const date = { year: 1394, month: 12, day: 30 };
-      expect(adapter.isValid(date)).to.be.false;
+      expect(jalaliAdapter.isValid(date)).to.be.false;
     });
     it('should return true when input is (1395, 12, 30)', () => {
       const date = { year: 1395, month: 12, day: 30 };
-      expect(adapter.isValid(date)).to.be.true;
+      expect(jalaliAdapter.isValid(date)).to.be.true;
     });
   });
 
   describe('isLeap', () => {
     it('should return false when input is 1394', () => {
-      expect(adapter.isLeap(1394)).to.be.false;
+      expect(jalaliAdapter.isLeap(1394)).to.be.false;
     });
     it('should return true when input is 1395', () => {
-      expect(adapter.isLeap(1395)).to.be.true;
+      expect(jalaliAdapter.isLeap(1395)).to.be.true;
     });
     it('should return false when input is 1396', () => {
-      expect(adapter.isLeap(1396)).to.be.false;
+      expect(jalaliAdapter.isLeap(1396)).to.be.false;
     });
   });
 
   describe('getMonthName', () => {
     it('should return string when number is between 1 and 12', () => {
       for (let i = 1; i <= 12; i += 1) {
-        expect(adapter.getMonthName(i)).to.be.a('string');
+        expect(jalaliAdapter.getMonthName(i)).to.be.a('string');
       }
     });
     it('should throw error when number is 0', () => {
-      expect(() => { adapter.getMonthName(0); }).to.throw(Error);
+      expect(() => { jalaliAdapter.getMonthName(0); }).to.throw(Error);
     });
     it('should throw error when number is 13', () => {
-      expect(() => { adapter.getMonthName(13); }).to.throw(Error);
+      expect(() => { jalaliAdapter.getMonthName(13); }).to.throw(Error);
     });
   });
 
   describe('getMonthLength', () => {
     it('should return 31 when input is (1396, 1) to (1396, 6)', () => {
       for (let i = 1; i <= 6; i += 1) {
-        expect(adapter.getMonthLength(1396, i)).to.equal(31);
+        expect(jalaliAdapter.getMonthLength(1396, i)).to.equal(31);
       }
     });
     it('should return 30 when input is (1396, 7) to (1396, 11)', () => {
       for (let i = 7; i <= 11; i += 1) {
-        expect(adapter.getMonthLength(1396, i)).to.equal(30);
+        expect(jalaliAdapter.getMonthLength(1396, i)).to.equal(30);
       }
     });
     it('should return 29 when input is (1396, 12)', () => {
-      expect(adapter.getMonthLength(1396, 12)).to.equal(29);
+      expect(jalaliAdapter.getMonthLength(1396, 12)).to.equal(29);
     });
     it('should return 30 when input is (1395, 12)', () => {
-      expect(adapter.getMonthLength(1395, 12)).to.equal(30);
+      expect(jalaliAdapter.getMonthLength(1395, 12)).to.equal(30);
     });
   });
 });
