739ff86b4a322a565813d1a3bde32943778268ec
Dariush Alipour <drsh.alipour@gmail.com>
2017-04-23 09:47:15 +0430

    updated package exports

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const adapter = require('./src/jalali.adapter.js');
+const JalaliAdapter = require('./src/jalali.adapter.js').Adapter;
 
-exports.name = adapter.name;
-exports.l10n = adapter.l10n;
-exports.i18n = adapter.i18n;
-exports.isValid = adapter.isValid;
-exports.isLeap = adapter.isLeap;
-exports.getMonthName = adapter.getMonthName;
-exports.getMonthLength = adapter.getMonthLength;
+exports.Adapter = JalaliAdapter;
