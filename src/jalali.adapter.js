ee91cebb49f0b861c3bfcd19a95627099a4fc707
Dariush Alipour <drsh.alipour@gmail.com>
2017-04-29 22:16:45 +0430

    added offset methods

--- src/jalali.adapter.js
+++ src/jalali.adapter.js
@@ -53,7 +53,56 @@ const Adapter = function Adapter() {
 
   const isLeap = year => jalaaliJs.isLeapJalaaliYear(year);
 
-  return { id, name, description, l10n, i18n, isValid, isLeap, getMonthName, getMonthLength };
+  const offsetYear = (date, offset) => ({
+    year: date.year + offset,
+    month: date.month,
+    day: date.day,
+  });
+
+  const offsetMonth = (date, offset) => {
+    /*
+      this method is not 100% accurate enough.
+      developer should write a custom month traversing function for the adapter
+    */
+    const i18nDate = i18n({ year: date.year, month: date.month, day: date.day });
+    const jsDate = new Date(`${i18nDate.year}-${i18nDate.month}-${i18nDate.day}`);
+    jsDate.setMonth(jsDate.getMonth() + offset);
+    return l10n({
+      year: jsDate.getFullYear(),
+      month: jsDate.getMonth() + 1,
+      day: jsDate.getDate(),
+    });
+  };
+
+  const offsetDay = (date, offset) => {
+    /*
+      this method is not 100% accurate enough.
+      developer should write a custom day traversing function for the adapter
+    */
+    const i18nDate = i18n({ year: date.year, month: date.month, day: date.day });
+    const jsDate = new Date(`${i18nDate.year}-${i18nDate.month}-${i18nDate.day}`);
+    jsDate.setDate(jsDate.getDate() + offset);
+    return l10n({
+      year: jsDate.getFullYear(),
+      month: jsDate.getMonth() + 1,
+      day: jsDate.getDate(),
+    });
+  };
+
+  return {
+    id,
+    name,
+    description,
+    l10n,
+    i18n,
+    isValid,
+    isLeap,
+    getMonthName,
+    getMonthLength,
+    offsetYear,
+    offsetMonth,
+    offsetDay,
+  };
 };
 
 exports.Adapter = Adapter;
